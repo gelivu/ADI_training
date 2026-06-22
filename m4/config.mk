cf6a0f93653290570e06b2a946987addcf832854
Aya Doctor <angelicacielodoctor@gmail.com>
2026-06-22 14:15:24 +0800

    Initial commit for Day1_Aya-2 project

---
+++ m4/config.mk
@@ -0,0 +1,11 @@
+# Configuration for MAX78000-CTBGA.
+#
+# This file was generated using Analog Devices CodeFusion Studio.
+# https://github.com/analogdevicesinc/codefusion-studio
+#
+# Generated at: 2026-06-22T04:14:21.680Z 
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2026 Analog Devices, Inc.
+
+
